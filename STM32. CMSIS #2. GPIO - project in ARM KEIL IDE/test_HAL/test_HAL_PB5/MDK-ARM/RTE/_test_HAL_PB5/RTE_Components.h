433fd6eebeb0044ddcef33d85c3167bb8373cce7
NR.electronics <48318430+nr-electronics@users.noreply.github.com>
2021-04-03 14:59:17 +0300

    CMSIS_GPIO_STM32

---
+++ STM32. CMSIS #2. GPIO - project in ARM KEIL IDE/test_HAL/test_HAL_PB5/MDK-ARM/RTE/_test_HAL_PB5/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'test_HAL_PB5' 
+ * Target:  'test_HAL_PB5' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ STM32. CMSIS #2. GPIO - project in ARM KEIL IDE/test_HAL/test_HAL_PB5/MDK-ARM/test_HAL_PB5/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+test_hal_pb5\startup_stm32f103xb.o: startup_stm32f103xb.s
